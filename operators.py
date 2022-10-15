bb8ec382d4efa4abcaceb319b5bb5e0c7d06024a
jpranjall <115860841+jpranjall@users.noreply.github.com>
2022-10-15 11:51:28 +0530

    Added exception.h.py
    It contains exception

---
+++ operators.py
@@ -0,0 +1,27 @@
+#Arithmetic operators
+#a=3
+#b=4
+#print("the value of 3+4 is,3-4")
+#print("the value of 3-4 is,3-4")
+#print("the value of 3/4 is,3/4")
+#print("the value of 3*4 is,3*4")
+
+#Assignment operators
+#a=4
+#a+=2
+#print('a')
+#print('b')
+#print('pranju')
+
+#comparison operators
+#b=(4>7)
+#print(b)
+
+#logical operators
+#from operator import truediv
+
+
+#bool1=true
+#bool2=False
+#print("the value of bool1,bool2 is")
+
